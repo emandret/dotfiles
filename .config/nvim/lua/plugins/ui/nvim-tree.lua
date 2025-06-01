a00a6ad0209e798bbdba2f990d7b08443a93a646
Edwy MANDRET <emandret@student.42.fr>
2025-06-01 20:52:21 +0100

    made nvim-tree window floating

--- .config/nvim/lua/plugins/ui/nvim-tree.lua
+++ .config/nvim/lua/plugins/ui/nvim-tree.lua
@@ -5,8 +5,10 @@ return {
       { "<Leader>e", "<Cmd>NvimTreeToggle<CR>", desc = "Toggle File Explorer" },
     },
     opts = {
-      sync_root_with_cwd = true,
+      disable_netrw = true,
+      hijack_netrw = true,
       respect_buf_cwd = true,
+      sync_root_with_cwd = true,
       update_focused_file = {
         enable = true,
         update_root = true,
@@ -14,14 +16,35 @@ return {
       sort = {
         sorter = "case_sensitive",
       },
-      view = {
-        width = 40,
-      },
       renderer = {
         group_empty = true,
       },
-      filters = {
-        dotfiles = true,
+      view = {
+        relativenumber = true,
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * 0.8
+            local window_h = screen_h * 0.8
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * 0.8)
+        end,
       },
       on_attach = function(bufnr)
         local api = require("nvim-tree.api")
