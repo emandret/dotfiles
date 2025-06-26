3b25f44f5c7b0b7724a59e72f9d36e7b73c64012
Edwy M <16176054+emandret@users.noreply.github.com>
2025-06-26 19:37:37 +0100

    Update autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -47,6 +47,19 @@ autocmd("FileType", {
 })
 
 -- -----------------------------------------------------------------------------
+-- LSP CLIENT CONFIG
+-- -----------------------------------------------------------------------------
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true, { bufnr = 0 })
+    end
+  end,
+})
+
+-- -----------------------------------------------------------------------------
 -- FORMATTERS
 -- -----------------------------------------------------------------------------
 
