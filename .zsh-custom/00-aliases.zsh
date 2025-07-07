2b14f3c94b8a6521148ca3566be8fc458275b3ed
Edwy M <16176054+emandret@users.noreply.github.com>
2025-07-07 12:00:22 +0100

    Delete .zsh-custom directory

--- .zsh-custom/00-aliases.zsh
+++
@@ -1,9 +0,0 @@
-unalias kga 2>/dev/null
-unalias kge 2>/dev/null
-
-alias kubectl='kubecolor'
-
-alias ip='ip -c'
-alias myip='dig +short myip.opendns.com @resolver4.opendns.com'
-
-alias clang-format-all='f(){ clang-tidy **/*.{c,h} -fix -checks="*" -- $ARCHFLAGS && clang-format -i -style=file **/*.{c,h}; unset -f f; }'
