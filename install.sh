880a4bca44c1f4215819fa4815913608932c41b3
Edwy MANDRET <emandret@student.42.fr>
2025-07-26 15:15:58 +0100

    added p10k

--- install.sh
+++ install.sh
@@ -63,6 +63,7 @@ zsh() {
 
 	git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 	cp .zshrc ~
 	cp .zsh_aliases.zsh ~
