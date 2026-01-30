1454da3bd08fe25ca344f9234061ab9e0e75ea80
Ed M <16176054+emandret@users.noreply.github.com>
2026-01-30 18:08:50 +0000

    Update dotfiles_install.sh

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -6,14 +6,15 @@ set -o pipefail
 cd -- "$(dirname -- "$(readlink -f -- "$0")")" >/dev/null 2>&1
 
 dotfiles_run_function() {
-  local component=$1
+  local name=$1
   local func=$2
-  shift 2
+  local opts=$3
+  shift 3
 
-  script="./dotfiles_install.d/$component.sh"
+  script="./dotfiles_install.d/$name.sh"
 
   if [[ ! -f "$script" ]]; then
-    echo "$script not found" >&2
+    echo "Error: $script not found" >&2
     return 1
   fi
 
@@ -22,33 +23,60 @@ dotfiles_run_function() {
     source "$script"
 
     if ! declare -f "$func" >/dev/null 2>&1; then
-      echo "$func not found in $script" >&2
+      echo "Error: $func not found in $script" >&2
       exit 1
     fi
 
-    "$func" "$@"
+    "$func" "$opts" "$@"
     exit $?
   )
 }
 
 dotfiles_can_install() {
-  local component=$1
-  shift 1
-
-  if ! dotfiles_run_function "$component" dotfiles_can_install >/dev/null 2>&1; then
+  if ! dotfiles_run_function "$1" 'dotfiles_can_install' >/dev/null 2>&1; then
     return 1
   fi
   return 0
 }
 
+parse_opt() {
+  case "$1" in
+    -h | --help)
+      echo '--help=true'
+      ;;
+    *)
+      echo "Error: invalid option: $1" >&2
+      exit 1
+      ;;
+  esac
+}
+
 if [[ $# -eq 0 ]]; then
-  printf "%-15s %-16s\n" COMPONENT_NAME CAN_BE_INSTALLED
+  printf "%-15s %-16s\n" 'COMPONENT_NAME' 'CAN_BE_INSTALLED'
   for file in ./dotfiles_install.d/*.sh; do
-    component="$(basename "$file" .sh)"
-    printf "%-15s %-16s\n" "$component" "$(dotfiles_can_install "$component" && echo yes || echo no)"
+    component_name="$(basename "$file" .sh)"
+    printf "%-15s %-16s\n" "$component_name" "$(dotfiles_can_install "$component_name" && echo 'yes' || echo 'no')"
   done
   exit 1
 fi
+
+opts=()
+args=()
+while (($# > 0)); do
+  case $1 in
+    --) # End of options
+      shift
+      break
+      ;;
+    -*)
+      opts+=("$(parse_opt "$1")")
+      ;;
+    *)
+      args+=("$1")
+      ;;
+  esac
+  shift
+done
 
 pids=()
 last_exit_code=0
@@ -56,39 +84,39 @@ last_exit_code=0
 logfile="$HOME/.dotfiles_install.log"
 rm -f "$logfile" && touch "$logfile"
 
-# kill the process group whose ID is equal to the PID of this shell
+# Kill the process group whose ID is equal to the PID of this shell
 trap 'kill -TERM -- -$$' EXIT
 
-for ((i = 1; i <= $#; i++)); do
-  component=${!i}
-  if dotfiles_can_install "$component"; then
-    dotfiles_run_function "$component" dotfiles_run_install >>"$logfile" 2>&1 &
-    pids[i]=$! # last background process PID
+for ((i = 1; i <= ${#args[@]}; i++)); do
+  component_name=${args[i]}
+  if dotfiles_can_install "$component_name"; then
+    dotfiles_run_function "$component_name" 'dotfiles_run_install' "${opts[*]:-}" "${args[@]:-}" >>"$logfile" 2>&1 &
+    pids[i]=$! # Last background process PID
   else
-    pids[i]=-1 # invalid PID
+    pids[i]=-1 # Invalid PID
   fi
 done
 
-printf "%-15s %-13s %9s\n" COMPONENT_NAME IS_INSTALLED EXIT_CODE
-for ((i = 1; i <= $#; i++)); do
-  component=${!i}
+printf "%-15s %-13s %9s\n" 'COMPONENT_NAME' 'IS_INSTALLED' 'EXIT_CODE'
+for ((i = 1; i <= ${#args[@]}; i++)); do
+  component_name=${args[i]}
   pid=${pids[i]}
 
-  if [[ $pid -lt 0 ]]; then
-    printf "%-15s %-13s %9s\n" "$component" skipped -
+  if ((pid < 0)); then
+    printf "%-15s %-13s %9s\n" "$component_name" 'skipped' '-'
     continue
   fi
 
   {
-    wait "$pid"
+    wait $pid
     exit_code=$?
   } || true
 
-  if [[ $exit_code -ne 0 ]]; then
-    printf "%-15s %-13s %9s\n" "$component" error "$exit_code"
+  if ((exit_code != 0)); then
+    printf "%-15s %-13s %9s\n" "$component_name" 'error' $exit_code
     last_exit_code=$exit_code
   else
-    printf "%-15s %-13s %9s\n" "$component" yes 0
+    printf "%-15s %-13s %9s\n" "$component_name" 'yes' 0
   fi
 done
 
