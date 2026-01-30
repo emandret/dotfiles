c88695b84560db3e87a3b33a2187f5ce35cf5bae
Ed M <16176054+emandret@users.noreply.github.com>
2026-01-30 18:18:10 +0000

    Update dotfiles_install.sh

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -8,8 +8,7 @@ cd -- "$(dirname -- "$(readlink -f -- "$0")")" >/dev/null 2>&1
 dotfiles_run_function() {
   local name=$1
   local func=$2
-  local opts=$3
-  shift 3
+  shift 2
 
   script="./dotfiles_install.d/$name.sh"
 
@@ -27,7 +26,7 @@ dotfiles_run_function() {
       exit 1
     fi
 
-    "$func" "$opts" "$@"
+    "$func" "$@"
     exit $?
   )
 }
@@ -39,18 +38,6 @@ dotfiles_can_install() {
   return 0
 }
 
-parse_opt() {
-  case "$1" in
-    -h | --help)
-      echo '--help=true'
-      ;;
-    *)
-      echo "Error: invalid option: $1" >&2
-      exit 1
-      ;;
-  esac
-}
-
 if [[ $# -eq 0 ]]; then
   printf "%-15s %-16s\n" 'COMPONENT_NAME' 'CAN_BE_INSTALLED'
   for file in ./dotfiles_install.d/*.sh; do
@@ -60,37 +47,19 @@ if [[ $# -eq 0 ]]; then
   exit 1
 fi
 
-opts=()
-args=()
-while (($# > 0)); do
-  case $1 in
-    --) # End of options
-      shift
-      break
-      ;;
-    -*)
-      opts+=("$(parse_opt "$1")")
-      ;;
-    *)
-      args+=("$1")
-      ;;
-  esac
-  shift
-done
-
-pids=()
-last_exit_code=0
-
 logfile="$HOME/.dotfiles_install.log"
 rm -f "$logfile" && touch "$logfile"
 
 # Kill the process group whose ID is equal to the PID of this shell
 trap 'kill -TERM -- -$$' EXIT
 
-for ((i = 1; i <= ${#args[@]}; i++)); do
-  component_name=${args[i]}
+pids=()
+last_exit_code=0
+
+for ((i = 1; i <= $#; i++)); do
+  component_name=${!i}
   if dotfiles_can_install "$component_name"; then
-    dotfiles_run_function "$component_name" 'dotfiles_run_install' "${opts[*]:-}" "${args[@]:-}" >>"$logfile" 2>&1 &
+    dotfiles_run_function "$component_name" 'dotfiles_run_install' >>"$logfile" 2>&1 &
     pids[i]=$! # Last background process PID
   else
     pids[i]=-1 # Invalid PID
@@ -98,8 +67,8 @@ for ((i = 1; i <= ${#args[@]}; i++)); do
 done
 
 printf "%-15s %-13s %9s\n" 'COMPONENT_NAME' 'IS_INSTALLED' 'EXIT_CODE'
-for ((i = 1; i <= ${#args[@]}; i++)); do
-  component_name=${args[i]}
+for ((i = 1; i <= $#; i++)); do
+  component_name=${!i}
   pid=${pids[i]}
 
   if ((pid < 0)); then
